b4436b77573e964f2326f78ca4f2848b19b9d08a
Alpinos <kostyaivanov353@gmail.com>
2024-07-03 12:35:41 +0300

    Rofls

--- backend/recordings/router.py
+++ backend/recordings/router.py
@@ -1,23 +1,41 @@
 # API routes
 from fastapi import APIRouter, Form, UploadFile, File
 
+
 # ROUTER INIT
 router = APIRouter()
+
+# SEND FILE TO S3
+async def SendToS3(SendFile: bytes):
+    return '...'
+
+# GET USER ID
+async def GetUserId(Token: str):
+    return 0
+
+# GET FILE FROM S3
+async def GetFileUrlFromS3(FileId: int, UserId: int):
+    return "..."
+
+# SOUND FILTRATION
+async def SoundFiltration(FileUrl: str):
+    return 0
 
 # GET FILE
 @router.get('/RecordingFile')
 async def GetFile(VkToken: str = Form(), FileId: int = Form()):
-    return 0
+    url = GetFileUrlFromS3(FileId, await GetUserId(VkToken))
+    return url
 
 # POST FILE
 @router.post('/RecordingFile')
-async def PostFile(File: UploadFile = File(), VkToken: str = Form()):
-    Write = open(File.filename, 'wb')
-    Write.write(await File.read())
-    Write.close()
+async def PostFile(AudioFile: UploadFile = File(), VkToken: str = Form()):
+    url = await SendToS3(await AudioFile.read())
+    UId = await GetUserId(VkToken)
     return 0
 
 # CHANGE FILE
 @router.put('/RecordingFile')
 async def PutFile(VkToken: str = Form(), FileId: int = Form()):
+    FileBytes = SoundFiltration(await GetFileUrlFromS3(FileId, await GetUserId(VkToken)))
     return 0
