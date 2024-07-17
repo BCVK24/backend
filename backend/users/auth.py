957a653a25fd8d84a5e4059907ad37a16870094d
gleb <gl.anohin@yandex.ru>
2024-07-17 15:13:32 +0300

    Finally added good authentication with all checks

--- backend/users/auth.py
+++ backend/users/auth.py
@@ -1,6 +1,7 @@
 import base64
 import hashlib
 import hmac
+import time
 
 from fastapi import HTTPException, Depends, Security
 from fastapi.security import APIKeyHeader
@@ -29,15 +30,20 @@ async def get_current_user(token: str = Security(api_key_header), session: Async
         data = i.split('=')
         args[data[0]] = data[1]
 
-    #sign_args = '&'.join([f'{i}={args[i]}' for i in args.keys() if i.startswith('vk_')])
+    sign_args = '&'.join([f'{i}={args[i]}' for i in args.keys() if i.startswith('vk_')])
 
-    #hashed = base64.b64encode(
-    #    hmac.new(settings.CLIENT_SECRET.encode(), sign_args.encode(), hashlib.sha256).digest()
-    #).decode().replace('+', '-').replace('/', '_').replace('=', '')
+    hashed = base64.b64encode(
+       hmac.new(settings.CLIENT_SECRET.encode(), sign_args.encode(), hashlib.sha256).digest()
+    ).decode().replace('+', '-').replace('/', '_').replace('=', '')
 
-    #if hashed != args['sign']:
-    #if False:
-        #raise HTTPException(401)
+    args['sign'] = args['sign'].strip()
+    hashed = hashed.strip()
+
+    if hashed[:len(args['sign'])] != args['sign']:
+        raise HTTPException(401)
+
+    if time.time() - int(args['vk_ts']) >= 60 * 60:
+        raise HTTPException(401)
 
     user_id = str(args['vk_user_id'])
 
