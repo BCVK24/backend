13e31b60953ff5342e83cada51cba0a490a72df9
Alpinos <kostyaivanov353@gmail.com>
2024-07-20 14:25:05 +0300

    Fix some bugs

--- backend/recordings/router.py
+++ backend/recordings/router.py
@@ -104,7 +104,7 @@ async def upload_recording(user: User = Depends(get_current_user), recording: st
 
 @router.post('/model_tags/{recording_id}')
 async def get_model_tags(recording_id: int, user: User = Depends(get_current_user),
-                         session: AsyncSession = Depends(get_session)) -> RecordingRead:
+                         session: AsyncSession = Depends(get_session)) -> RecordingRel:
     recording = await session.get(Recording, recording_id)
 
     if not recording:
@@ -125,12 +125,12 @@ async def get_model_tags(recording_id: int, user: User = Depends(get_current_use
 
     await session.commit()
 
-    return RecordingRead.model_validate(recording, from_attributes=True)
+    return RecordingRel.model_validate(recording, from_attributes=True)
 
 
 @router.delete('/model_tags/{recording_id}')
 async def delete_model_tags(recording_id: int, user: User = Depends(get_current_user),
-                         session: AsyncSession = Depends(get_session)) -> RecordingRead:
+                         session: AsyncSession = Depends(get_session)) -> RecordingRel:
     recording = await session.get(Recording, recording_id)
 
     if not recording:
@@ -143,4 +143,4 @@ async def delete_model_tags(recording_id: int, user: User = Depends(get_current_
     Tag.delete_model_tag_by_recording_id(recording_id)
     await session.commit()
 
-    return RecordingRead.model_validate(recording, from_attributes=True)
+    return RecordingRel.model_validate(recording, from_attributes=True)
