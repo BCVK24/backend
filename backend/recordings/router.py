1dca3d54d7eb535ecb53b4ebf13ec8b2d40fc6b5
gleb <gl.anohin@yandex.ru>
2024-07-22 11:51:18 +0300

    Fixed db

--- backend/recordings/router.py
+++ backend/recordings/router.py
@@ -66,7 +66,12 @@ async def put_recording_name(recording: RecordingUpdate, user: User = Depends(ge
 @router.get('/{recording_id}')
 async def get_recording(recording_id: int, user: User = Depends(get_current_user),
                         session: AsyncSession = Depends(get_session)) -> RecordingRel:
-    recording = await session.get(Recording, recording_id)
+    query = (
+        select(Recording).filter(Recording.id == recording_id)
+        .join(Recording.tags).filter(Tag.tag_type != TagType.SOURCETAG)
+    )
+
+    recording = await session.scalar(query)
 
     if not recording:
         raise HTTPException(404)
